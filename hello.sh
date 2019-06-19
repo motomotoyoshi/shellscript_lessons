151c25263204a1b1e3475825a9fbcf6b7c7b5bb5
motomotoyoshi <motomoto.git@gmail.com>
2019-06-19 16:10:33 +0000

    update hello.sh

--- hello.sh
+++ hello.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-echo "hello $1, $2"
-echo $0
-echo $#
-echo $@
+#read name
+#echo "hello $name"
+
+#read -p "Name: " name
+#echo "hello $name"
+
+read -p "Pick 3 colors: " c1 c2 c3
+echo $c1
+echo $c2
+echo $c3
